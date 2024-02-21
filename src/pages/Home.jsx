c52e98df69839876e98b46910741cf63b5158285
NovaeChan <novaestar@proton.me>
2024-02-21 21:46:47 +0100

    Fixed scolling animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import '../styles/main.css'
 import $ from 'jquery'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
 import { useEffect } from 'react';
 
 export default function Home() {
-    //Add scale to show what is a million when showing Bernard Arnault wealth
-    //Add trivia to explain how stupid it is. Like how long someone at minimum wage would need to work to get this amount of wealth
 
     useEffect(() => {
         let currentIndex = 0;
@@ -33,64 +33,94 @@ export default function Home() {
             return indexScroll(currentIndex);
         })
 
+        window.addEventListener('scroll', () => { 
+            updateCounter();
+        })
+
         function indexScroll(idx){
             
             let active = document.querySelector('.active');
-            if(active == undefined) document.querySelector('.tenPoorest').add('active');
+            if(active == undefined) document.querySelector('.tenPoorestBlock').add('active');
             let x = sections[idx];
-            console.log(x);
+
             active.classList.remove('active');
             x.classList.add('active');
+            let elt = $('.active');
+            let eltOffset = elt.offset().top;
+            let eltHeight = elt.height();
+            const windowHeight = $(window).height();
+            let offset = 0;
+            if(eltHeight < windowHeight) {
+                offset = eltOffset - ((windowHeight / 2) - (eltHeight / 2));
+            }
+            else { 
+                offset = eltOffset;
+            }
+
             $("html, body").animate({
-                scrollTop: $(".active").offset().top
-            }, 2000);
+                scrollTop: offset,
+            }, 4000);
         }
+
+        function updateCounter() { 
+            if(window.scrollY > document.querySelector('.bernard').offsetTop){
+                document.querySelector('.counter').innerHTML =  window.scrollY * 342000;
+            }
+            else { 
+                document.querySelector('.counter').innerHTML = '';
+            }
+        }
+
+
     }, [])
 
 
     return (
         <>
             <header>
-                <h1>Visualisation richesse</h1>
+                <h1>Visu de la richesse</h1>
             </header>
             <main>
-                <div className='main-title'>
-                    <h2>Que représente la richesse des <span className='main-title-word'>milliardaires</span> comparée au français moyen ?</h2>
-                    <span>Avec l&apos;augmentation des inégalités d&apos;années en années, il est temps de rappeler le patrimoine en euros de Français les plus riches comparé à celui du français moyen.</span>
-                </div>
-                <div className='main-desc'>
-                    <p>Chaque pixel bleu correspond à 1000€.</p>
-                    <p>Pour défiler chaque catégorie, il suffit de cliquer sur la flèche qui va apparaître</p>
+                <div className='wrapper-head'>
+                    <div className='main-title'>
+                        <h2>Que représente la richesse des <span className='main-title-word'>milliardaires</span> comparée au français moyen ?</h2>
+                        <span>Avec l&apos;augmentation des inégalités d&apos;années en années, il est temps de rappeler le patrimoine en euros de Français les plus riches comparé à celui du français moyen.</span>
+                    </div>
+                    <div className='main-desc'>
+                        <p>Chaque pixel bleu correspond à 1000€.</p>
+                        <p>Pour défiler chaque catégorie, il suffit de cliquer sur la flèche qui va apparaître</p>
+                    </div>
                 </div>
                 <div className='wrapper'>
-                    <div className='demo tenPoorest active'>
-                        <span>Les 10% les plus pauvres possèdent au plus 4400€ </span>
+                    <div className='counter'></div>
+                    <div className='demo demoWrapper tenPoorestBlock active'>
+                        <div><span>Les 10% les plus pauvres possèdent au plus 4400€ </span></div>
                         <div className='money-block tenPoorest'></div>
                     </div>
-                    <div className='demo'>
-                        <span>177 200€ le patrimoine médian en France</span>
+                    <div className='demoWrapper'>
+                        <div><span>177 200€ le patrimoine médian en France</span></div>
                         <div className='money-block median'></div>
                     </div>
-                    <div className='demo'>
-                        <span>Les 10% les plus riches de France (716 300€)</span>
+                    <div className='demoWrapper'>
+                        <div><span>Les 10% les plus riches de France (716 300€)</span></div>
                         <div className='money-block tenRichest'></div>
                     </div>
-                    <div className='demo'>
-                        <span>On arrive aux millionaires.<br/> Les 5% les plus riches de France (1 034 600€)</span>
+                    <div className='demoWrapper'>
+                        <div><span>On arrive aux millionaires.<br/> Les 5% les plus riches de France (1 034 600€)</span></div>
                         <div className='money-block fiveRichest'></div>
                     </div>
-                    <div className='demo'>
-                        <span>Les 1% les plus riches de France (2 239 200€)</span>
+                    <div className='demo demoWrapper'>
+                        <div><span>Les 1% les plus riches de France (2 239 200€)</span></div>
                         <div className='money-block oneRichest'></div>
                     </div>
-                    <div className='demo'>
-                        <span>Voici ce que représente un milliard d&apos;euros</span>
+                    <div className='demo demoWrapper'>
+                        <div><span>Voici ce que représente un milliard d&apos;euros</span></div>
                         <div className='money-block oneBillion'><div className='demo oneBillion-end'></div></div>
                         
                         
                     </div>
-                    <div className='demo'>
-                        <span>Voici la richesse de Bernard Arnault 221,1 milliards d&apos;euros</span>
+                    <div className='demo demoWrapper'>
+                        <div><span>Voici la richesse de Bernard Arnault 221,1 milliards d&apos;euros</span></div>
                         <div className='money-block bernard'>
                             <div className='demo information-patience'><span>On vient d&apos;atteindre seulement 15% de sa fortune</span></div>
                             <div className='demo information-reminder'><span>Pour rappel, voici ce que représente <br />un million d&apos;euros</span></div>
@@ -103,10 +133,10 @@ export default function Home() {
                             
                         </div>
                     </div>
-                    <div className='demo'>
+                    <div className='demoWrapper'>
                         <span>Ceci ne représente que la richesse de l&apos;homme le plus riche de France et du monde. Imaginez ce que pourrait représenter les 100 personnes les plus riches de France</span>
                     </div>
-                    <div className='demo'>
+                    <div className='demoWrapper'>
                         <h3>Sources :</h3>
                         <ul>
                             <li>Observatoire des inégalités</li>
@@ -116,8 +146,8 @@ export default function Home() {
                     </div>
                 </div>
                 <div className='wrapper-arrow'>
-                    <div className='arrow arrow-up' id='scrollUpArrow'><a href='#'>&#8593;</a></div>
-                    <div className='arrow arrow-down' id="scrollDownArrow"><a href='#/'>&#8595;</a></div>
+                    <div className='arrow arrow-up' id='scrollUpArrow'><FontAwesomeIcon icon={faChevronUp} /></div>
+                    <div className='arrow arrow-down' id="scrollDownArrow"><FontAwesomeIcon icon={faChevronDown} /></div>
                 </div>
             </main>
         </>
